a9292b413006c5b8e0906de77e58214d10383664
Tanay Bhor <bhortanay@gmail.com>
2025-06-19 21:56:33 +0530

    Initial commit

---
+++ src/Footer.jsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer style={{
+            display:"flex",
+            justifyContent:'center',
+        }}>
+            <p>Created by @TanayBhor</p>
+        </footer>
+    )
+}
+
+
+export default Footer
